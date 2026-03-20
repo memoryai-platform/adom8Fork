d1813472fd615817af4d2c9184a00847eb9aa365
toddpick <74226577+toddpick@users.noreply.github.com>
2026-03-20 08:32:56 -0600

    chore: update dashboard auth image

--- dashboard/src/components/LoginGate.jsx
+++ dashboard/src/components/LoginGate.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import AuthImage from '../mosaic/images/auth-image.jpg';
+import AuthImage from '../mosaic/images/auth-image.png';
 import { validateAppKey } from '../api';
 
 export default function LoginGate({ onAuthenticated }) {
--- dashboard/src/mosaic/pages/Signin.jsx
+++ dashboard/src/mosaic/pages/Signin.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import AuthImage from "../images/auth-image.jpg";
+import AuthImage from "../images/auth-image.png";
 
 function Signin() {
   return (
